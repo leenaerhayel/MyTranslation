7288fdbcec540457945dded665393f4c628c0292
Hackeru_Student <hackeru@hackeru-students-mac.local>
2018-05-03 00:01:32 -0700

    Initial Commit

---
+++ MyTranslation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTranslation
+//
+//  Created by Hackeru_Student on 5/3/18.
+//  Copyright © 2018 Leena Erhayel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
